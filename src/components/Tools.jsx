87c472ab40021ebeebff2098d1088306eda01b47
kunal23g <141353869+kunal23g@users.noreply.github.com>
2024-09-07 01:17:42 +0530

    Update Tools.jsx

--- src/components/Tools.jsx
+++ src/components/Tools.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import tools from "../assets/Tools.png"
 import { CircleArrowRight } from 'lucide-react'
+import { Link } from 'react-router-dom'
 
 
 
@@ -10,12 +11,12 @@ function Tools() {
 
         <div className=" col-span-1 p-3 flex flex-col justify-center ">
         <h1 className="text-3xl  text-dark-blue mb-4 ">Connect PACAM To  <br />Other Tools You Use.</h1>
-        <p className="text-xs w-[70%]  text-gray-600 mb-5">Easily  integrate it with your WMS (Warehouse Management System) or OMS (Order Management System) to maintain efficiency and create video los for your customers </p>
+        <p className="text-xm w-[70%]  text-gray-600 mb-5">Easily  integrate it with your WMS (Warehouse Management System) or OMS (Order Management System) to maintain efficiency and create video los for your customers </p>
 
-        <button className="flex hover:text-[#041558] mt-5">
+       <Link to="/contact" className="flex hover:text-[#041558] mt-5">
         Find More Solution Partners
         <CircleArrowRight size={20} className="ml-2" />
-      </button>
+        </Link>
             
          </div>
         <div className=" mt-2 col-span-1">
