18f15dbf3991a3b3a80f2280435f301427b0587c
kunal23g <141353869+kunal23g@users.noreply.github.com>
2024-09-07 00:19:14 +0530

    Update Testimonial.jsx

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -8,9 +8,9 @@ const Testimonials = () => {
         <div className=" p-2  max-w-[1240px] mx-auto  my-20   md:grid grid-cols-2 ">
 
         <div className=" col-span-1 p-3 lg:w-[80%] flex flex-col justify-center ">
-        <h6 className="text-xs text-gray-600 mb-2 ">Marketing : Enhance branding</h6>
+        <h6 className="text-xm text-gray-600 mb-2 ">Marketing : Enhance branding</h6>
         <h1 className="text-2xl font-bold text-dark-blue mb-4 ">Boost Your Brand's Personality</h1>
-        <p className="text-xs text-gray-600 mb-5">Deliver every customer experience uniquely unforgettable.</p>
+        <p className="text-xm text-gray-600 mb-5">Deliver every customer experience uniquely unforgettable.</p>
 
         <ul >
             <li className=" flex mb-2 text-md  text-justify" >
