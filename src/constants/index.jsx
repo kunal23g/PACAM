136f4d5ed1b8340c791316c690875c0bc0e8e494
kunal23g <141353869+kunal23g@users.noreply.github.com>
2024-09-07 01:14:44 +0530

    Update index.jsx

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -7,16 +7,16 @@ export const navItems = [
 
   export const featureItems1=[
     {text:"Simply connect to our new program and let our solution do the heavy lifting"},
-    {text:"No logistics system needed. Just dive into Inspection Modec"},
+    {text:"No logistics system needed. Just dive into Inspection Mode"},
     {text:"Effortlessly master your video searches with just an invoice number"},
     {text:"Just pop in the video URL and master the art of seamless communication"},
     {text:"Seamlessly integrate video into your app and watch revisits soar"}
   ]
   export const featureItems2=[
-    {text:"Effortlessly integrate our solution in any countryyz"},
+    {text:"Effortlessly integrate our solution in any country"},
     {text:"Save and access your videos from anywhere with seamless cloud storage"},
     {text:"Create and manage sub-accounts for all your departments"},
-    {text:"Access and manage footage from every department on our CMS.z"},
+    {text:"Access and manage footage from every department on our CMS."},
     {text:"Effortless relocation with our flexible installation options"}
   ]
   
