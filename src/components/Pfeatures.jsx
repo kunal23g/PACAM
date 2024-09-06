a0e1150b4a8232917ab1e906354bba5da4ce2e21
kunal23g <141353869+kunal23g@users.noreply.github.com>
2024-09-07 01:01:22 +0530

    Update Pfeatures.jsx

--- src/components/Pfeatures.jsx
+++ src/components/Pfeatures.jsx
@@ -8,6 +8,7 @@ import { CircleArrowRight } from 'lucide-react'
 import { featureItems1 } from "../constants"
 import { featureItems2 } from "../constants"
 import { featureItems3 } from "../constants"
+import { Link } from "react-router-dom"
 
 
 
@@ -37,31 +38,31 @@ const Pfeatures=()=> {
     return (
       <div className="my-20 grid grid-cols-1 lg:grid-cols-7 ">
       <div className="col-span-1 lg:col-span-4 justify-between bg-[#F7EFE5] p-6 lg:p-16">
-        <div className="mt-10 flex flex-nowrap justify-between gap-2">
+        <div className="mt-10 flex flex-nowrap justify-between lg:max-w-[80%] gap-2">
           <button onClick={() => handleImg(1)} 
             
-           className={`py-4 px-6 text-sm font-light
+           className={`py-4 px-6 text-sm font-light rounded-md
            ${activeButton===1 ?  'bg-[#041558] text-white' : 'hover:bg-[#041558] hover:text-white'}`}>
             PACAM IS SIMPLE
           </button>
 
 
           <button onClick={() => handleImg(2)}           
-           className={`py-4 px-6 text-sm font-light
+           className={`py-4 px-6 text-sm font-light rounded-md
            ${activeButton===2 ?  'bg-[#041558] text-white' : 'hover:bg-[#041558] hover:text-white'}`}>
             PACAM IS EASY
           </button>
 
 
           <button onClick={() => handleImg(3)}           
-           className={`py-4 px-6 text-sm font-light
+           className={`py-4 px-6 text-sm font-light rounded-md
            ${activeButton===3 ?  'bg-[#041558] text-white' : 'hover:bg-[#041558] hover:text-white'}`}>
             PACAM IS QUICK
           </button>
 
         </div>
     
-        <ul className="flex flex-col  justify-center p-5 lg:p-6 mt-10">
+        <ul className="flex flex-col lg:h-[200px] justify-center p-5 lg:p-6 mt-10">
           {Li.map((item, index) => (
             <li className="flex mb-2 text-md text-justify" key={index}>
               <img className="pr-2" src={icon} alt="" /> {item.text}
@@ -69,10 +70,11 @@ const Pfeatures=()=> {
           ))}
         </ul>
     
-        <button className="flex mt-5 py-4 px-6 hover:bg-[#041558] hover:text-white rounded-md bg-transparent">
+        <Link to="/contact"
+        className="inline-flex flex mt-5 py-4 px-6 hover:bg-[#041558] hover:text-white rounded-md bg-transparent">
           Learn More
           <CircleArrowRight size={20} className="ml-2" />
-        </button>
+          </Link> 
       </div>
     
       <div className="col-span-1 lg:col-span-3 w-full bg-[#F3E2D3] flex justify-center items-center">
