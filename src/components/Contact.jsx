252ab90559dbc8026ff1aaf11e256f813b499928
kunal23g <141353869+kunal23g@users.noreply.github.com>
2024-09-09 01:15:09 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,15 +5,15 @@ import { Link } from 'react-router-dom'
 
 const Contact=()=> {
     return (
-        <div className='max-w-[1420px] mx-auto py-10 items-center my-20 mx  flex flex-col bg-[#F3E2D3] '>
+        <div className='font-playfair max-w-[1420px] mx-auto py-10 items-center my-20 mx  flex flex-col bg-[#F3E2D3] '>
         <div className='mb-4  align-center p-5'>
-        <h1 className="text-2xl md:text-3xl lg:text-4xl text-dark-blue text-center">Pacam Is Designed For Business Of <br /> Any Size, Across All Industries And Locatoins.</h1>
+        <h1 className="text-2xl font-medium md:text-3xl lg:text-4xl text-dark-blue text-center">Pacam Is Designed For Business Of <br /> Any Size, Across All Industries And Locatoins.</h1>
 
         </div>
         <div className='  md:justify-center '>
             <Link
                 to="/contact"
-             className=" flex  justify-center py-[1rem] px-[1rem] mt-5 rounded-md   text-white bg-black">
+             className=" rounded-md flex  justify-center py-[1rem] px-[1rem] mt-5    text-white bg-black">
                 Contact Us<CircleArrowRight size={19} className="ml-2 mt-[2px] "  />
                 </Link>
             
