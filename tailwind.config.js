b3230be73ac780c6aeaac52040208dbc713d9dd1
kunal23g <141353869+kunal23g@users.noreply.github.com>
2024-09-09 00:38:54 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      transitionDelay: {
+        '1100': '1100ms',
+        '1400': '1400ms',
+      },
+      fontFamily: {
+        'sans': ['Roboto', 'sans-serif'],
+        'Guerrilla':['"Protest Guerrilla"', 'system-ui'],
+        'lora':['Lora', 'serif'],
+        'playfair':["Playfair Display", 'serif']
+      }
+    },
   },
   plugins: [],
 }
