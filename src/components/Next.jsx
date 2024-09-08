47d0d9f748ed5e5b6b19011077af6ddf6f7982a1
kunal23g <141353869+kunal23g@users.noreply.github.com>
2024-09-09 01:09:53 +0530

    Update Next.jsx

--- src/components/Next.jsx
+++ src/components/Next.jsx
@@ -5,22 +5,21 @@ import { Link } from 'react-router-dom'
 
 const Next=()=> {
     return (
-        <div className='max-w-[1240px] mx-auto py-10 pl-5  flex flex-col bg-indigo-200 bg-opacity-20 '>
+        <div className=' font-playfair max-w-[1240px] mx-auto py-10 pl-5  flex flex-col bg-indigo-200 bg-opacity-20 '>
             <div className='mb-4'>
-            <h6 className="text-xm text-gray-600 md:text-center mb-3">Ready to Take the Next Step?</h6>
+            <h6 className="text-xl text-gray-600 md:text-center mb-3">Ready to Take the Next Step?</h6>
             <h1 className="text-4xl text-dark-blue md:text-center">Set It Up And Start Using Pacam Today</h1>
 
             </div>
             <div className=' flex md:justify-center '>
                 <Link to="/contact"
-                className="rounded-md flex  justify-center py-[1rem] px-[1rem] mt-5    text-white bg-black">
+                className=" flex  justify-center py-[1rem] px-[1rem] mt-5    text-white bg-black">
                     Contact Us<CircleArrowRight size={20} className="ml-2 mt-[1px] "  />
                     </Link> 
                 <Link to="/contact"
-                className="rounded-md flex  text-center py-[1rem] px-[1rem] mt-5 ml-5   text-white bg-black">
+                className=" flex  text-center py-[1rem] px-[1rem] mt-5 ml-5   text-white bg-black">
                     Discover Now<CircleArrowRight size={20} className="ml-2 mt-[1px]"  />
                     </Link> 
-                
                 </div>
         </div>
     )
