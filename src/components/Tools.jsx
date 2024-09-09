6ea682fa44fefd35f3f89de20cbb6b4fb12300a1
kunal23g <141353869+kunal23g@users.noreply.github.com>
2024-09-09 16:04:39 +0530

    Update Tools.jsx

--- src/components/Tools.jsx
+++ src/components/Tools.jsx
@@ -43,10 +43,10 @@ function Tools() {
                 <h1 className="text-3xl font-playfair font-bold text-dark-blue mb-4">
                     Connect PACAM To <br />Other Tools You Use.
                 </h1>
-                <p className="text-md w-[70%] font-lora text-gray-600 mb-5">
+                <p className="text-xm  lg:text-lg w-[70%] font-lora text-gray-600 mb-5">
                     Easily integrate it with your WMS (Warehouse Management System) or OMS (Order Management System) to maintain efficiency and create video logs for your customers.
                 </p>
-                <Link to="/contact" className="font-lora flex items-center hover:text-[#041558] mt-5">
+                <Link to="/contact" className="text-xm  lg:text-lg font-lora flex items-center hover:text-[#041558] mt-5">
                     Find More Solution Partners
                     <CircleArrowRight size={20} className="ml-2" />
                 </Link>
